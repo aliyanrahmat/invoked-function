87c3e76fda5450ed7af887831f848a5788a70514
aliyan <aliyanrahmat6@gmail.com>
2023-07-23 01:43:27 +0500

    invoked function

---
+++ jason.js
@@ -0,0 +1,28 @@
+// .......invoked function.....
+
+
+
+let a = ()=>{ return new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve(456)
+    },4000)
+})
+}
+// let f = async()=>{
+//     let b = await a()
+//     console.log(b,)
+//     let c = await a() 
+//         console.log(c)
+//     let d = await a()
+//     console.log(d)
+// }
+// f()
+
+(async()=>{
+    let b = await a()
+    console.log(b,)
+    let c = await a() 
+        console.log(c)
+    let d = await a()
+    console.log(d)
+})()
